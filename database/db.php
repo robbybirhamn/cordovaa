d2cc7526d30de79a31414f6486146f224e86295e
robbybirham <robbybirhamn@gmail.com>
2020-03-26 00:13:47 +0700

    second commit update url

--- database/db.php
+++ database/db.php
@@ -1,4 +1,4 @@
 <?php
     header("Access-Control-Allow-Origin: *");
-    $con = mysqli_connect("localhost","root","","kursus") or die ("could not connect database");
+    $con =  mysqli_connect("localhost", "id12804356_root", "unisbankmania","id12804356_akademik");
 ?>
